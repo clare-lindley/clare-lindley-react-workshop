6756016b029894abc7119e67e0265d70343a09a1
Clare Lindley <clare.lindley@gmail.com>
2024-05-24 12:12:02 +0100

    Update Exercise 1 Solution.js

--- Exercise 1 Solution.js
+++ Exercise 1 Solution.js
@@ -1,1 +1,28 @@
+// Import the React library
+import React from 'react';
+
+// Create a functional component named Exercise1
+const Exercise1 = () => {
+  // Create an ordered list with three favorite animals - all cats, obviously!
+  const animalsList = React.createElement('ol', null,
+    React.createElement('li', null, 'Cat'),
+    React.createElement('li', null, 'Cat'),
+    React.createElement('li', null, 'Cat')
+  );
+
+  // Create a section with your name
+  const nameSection = React.createElement('section', null,
+    'Clare',
+    animalsList
+  );
+
+  // Wrap the section in a div element
+  const divElement = React.createElement('div', null, nameSection);
+
+  // Return the div element
+  return divElement;
+};
+
+// Export the Exercise1 component
+export default Exercise1;
 
